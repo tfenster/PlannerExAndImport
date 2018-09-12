a25d9380ea7a5c59edcaa3419bc547e04e968677
Tobias Fenster <tfenster@infoma.de>
2018-09-12 10:24:50 +0200

    show content

--- manifest.ps1
+++ manifest.ps1
@@ -1,4 +1,7 @@
 if (!(Test-Path ~/.docker)) { mkdir ~/.docker }
 '{ "experimental": "enabled" }' | Out-File ~/.docker/config.json -Encoding Ascii
-docker manifest create tobiasfenster/plannerexandimport:latest  tobiasfenster/plannerexandimport:linux tobiasfenster/plannerexandimport:windows
+
+Get-Content ~/.docker/config.json | foreach {Write-Output $_}
+
+docker manifest create tobiasfenster/plannerexandimport:latest tobiasfenster/plannerexandimport:linux tobiasfenster/plannerexandimport:windows
 docker manifest push tobiasfenster/plannerexandimport:latest 
