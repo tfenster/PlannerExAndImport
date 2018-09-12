554f77eb29fca0943ce51c4bd332b05e89949c62
Tobias Fenster <tfenster@infoma.de>
2018-09-12 10:42:38 +0200

    bring it back together

--- manifest.ps1
+++ manifest.ps1
@@ -1,9 +1,5 @@
 if (!(Test-Path ~/.docker)) { mkdir ~/.docker }
 '{ "experimental": "enabled" }' | Out-File ~/.docker/config.json -Encoding Ascii
 
-dir ~/.docker
-dir ~/.docker/config.json
-Get-Content ~/.docker/config.json | ForEach-Object {Write-Output $_}
-
 docker --config ~/.docker manifest create tobiasfenster/plannerexandimport:latest tobiasfenster/plannerexandimport:linux tobiasfenster/plannerexandimport:windows
-docker manifest push tobiasfenster/plannerexandimport:latest 
+docker --config ~/.docker manifest push tobiasfenster/plannerexandimport:latest 
