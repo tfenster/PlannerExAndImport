c222a71c866d25e74ba792782ef41103c96134f7
Tobias Fenster <tfenster@infoma.de>
2018-09-12 16:06:10 +0200

    not needed anymore

--- manifest.ps1
+++
@@ -1,5 +0,0 @@
-if (!(Test-Path ~/.docker)) { mkdir ~/.docker }
-'{ "experimental": "enabled" }' | Out-File ~/.docker/config.json -Encoding Ascii
-
-docker --config ~/.docker manifest create tobiasfenster/plannerexandimport:latest tobiasfenster/plannerexandimport:linux tobiasfenster/plannerexandimport:windows
-docker --config ~/.docker manifest push tobiasfenster/plannerexandimport:latest 
