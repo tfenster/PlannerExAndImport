3af29b8c68c07cf7afdbac4d89775952005e80b4
Tobias Fenster <tfenster@infoma.de>
2018-09-12 10:28:32 +0200

    explicitly use config

--- manifest.ps1
+++ manifest.ps1
@@ -3,5 +3,5 @@ if (!(Test-Path ~/.docker)) { mkdir ~/.docker }
 
 Get-Content ~/.docker/config.json | foreach {Write-Output $_}
 
-docker manifest create tobiasfenster/plannerexandimport:latest tobiasfenster/plannerexandimport:linux tobiasfenster/plannerexandimport:windows
+docker --config ~/.docker/config.json manifest create tobiasfenster/plannerexandimport:latest tobiasfenster/plannerexandimport:linux tobiasfenster/plannerexandimport:windows
 docker manifest push tobiasfenster/plannerexandimport:latest 
